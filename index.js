6f1fbc500fd3a6580e3730704cfec929e70d066b
Augusto Riccardi <augustoriccardi@gmail.com>
2024-03-15 14:39:29 -0300

    configuración PORT -  Railway

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ app.post("/promptior-chatbot", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor en ejecución en el puerto ${PORT}`);
 });
