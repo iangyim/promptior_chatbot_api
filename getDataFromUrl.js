65d9256b859419103cb9701444a01ded39808ffe
Augusto Riccardi <augustoriccardi@gmail.com>
2024-03-19 11:55:30 -0300

    prueba  ignoreDefaultArgs: ["--disable-extensions"]

--- getDataFromUrl.js
+++ getDataFromUrl.js
@@ -1,12 +1,16 @@
 import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+import puppeteer from "puppeteer";
 
 export const getDataFromUrl = async (url) => {
   console.log(`Haciendo la llamada a: ${url}`);
 
+  const browser = await puppeteer.launch({
+    ignoreDefaultArgs: ["--disable-extensions"], // Aquí se especifica la bandera --disable-extensions
+    headless: "new", // Otras opciones de lanzamiento de Puppeteer
+  });
+
   const loader = new PuppeteerWebBaseLoader(url, {
-    launchOptions: {
-      headless: "new",
-    },
+    puppeteer: browser,
     async evaluate(page, browser) {
       try {
         await page.goto(url, { waitUntil: "networkidle0" });
@@ -17,11 +21,14 @@ export const getDataFromUrl = async (url) => {
         return textContent || "";
       } catch (error) {
         console.error("Ocurrió un error al cargar la página", error);
-        await browser.close();
         return "";
       }
     },
   });
 
-  return await loader.load();
+  const data = await loader.load();
+
+  await browser.close();
+
+  return data;
 };
