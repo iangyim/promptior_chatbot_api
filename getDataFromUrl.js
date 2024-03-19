103437eacd47ae06fbed89a8eccf7c020f6a1bcd
Augusto Riccardi <augustoriccardi@gmail.com>
2024-03-19 20:08:44 -0300

    prueba6

--- getDataFromUrl.js
+++ getDataFromUrl.js
@@ -1,13 +1,37 @@
 import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+import { launch as launchPuppeteerCore } from "puppeteer-core";
+import puppeteer from "puppeteer";
+
+let chrome = {};
+let puppeteerInstance;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = await import("chrome-aws-lambda").then((mod) => mod.default);
+  puppeteerInstance = launchPuppeteerCore;
+} else {
+  puppeteerInstance = puppeteer.launch;
+}
 
 export const getDataFromUrl = async (url) => {
   console.log(`Haciendo la llamada a: ${url}`);
 
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  const browser = await puppeteerInstance(options);
+
   const loader = new PuppeteerWebBaseLoader(url, {
-    launchOptions: {
-      headless: "new",
-    },
-    async evaluate(page, browser) {
+    launchOptions: options,
+    async evaluate(page) {
       try {
         await page.goto(url, { waitUntil: "networkidle0" });
         const textContent = await page.evaluate(() => {
@@ -17,11 +41,13 @@ export const getDataFromUrl = async (url) => {
         return textContent || "";
       } catch (error) {
         console.error("Ocurrió un error al cargar la página", error);
-        await browser.close();
         return "";
       }
     },
   });
 
-  return await loader.load();
+  const result = await loader.load();
+  await browser.close(); // Cerrar el navegador después de usarlo
+
+  return result;
 };
