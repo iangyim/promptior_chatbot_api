8b630f9d2924e825cf4677230e9a38a95f9de39c
Augusto Riccardi <augustoriccardi@gmail.com>
2024-03-22 09:28:24 -0300

    actualización

--- getDataFromUrl.js
+++ getDataFromUrl.js
@@ -1,27 +1,57 @@
+import dotenv from "dotenv";
 import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+import puppeteer from "puppeteer";
+
+dotenv.config();
 
 export const getDataFromUrl = async (url) => {
   console.log(`Haciendo la llamada a: ${url}`);
 
-  const loader = new PuppeteerWebBaseLoader(url, {
-    launchOptions: {
-      headless: "new",
-    },
-    async evaluate(page, browser) {
-      try {
-        await page.goto(url, { waitUntil: "networkidle0" });
-        const textContent = await page.evaluate(() => {
-          const bodyElement = document.querySelector("body");
-          return bodyElement ? bodyElement.textContent : "";
-        });
-        return textContent || "";
-      } catch (error) {
-        console.error("Ocurrió un error al cargar la página", error);
-        await browser.close();
-        return "";
-      }
-    },
-  });
+  let browser;
+  try {
+    // Configuración del navegador
+    browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
 
-  return await loader.load();
+    // Configuración del loader
+    const loader = new PuppeteerWebBaseLoader(url, {
+      launchOptions: {
+        headless: process.env.NODE_ENV === "production",
+      },
+      async evaluate(page, browser) {
+        try {
+          await page.goto(url, { waitUntil: "networkidle0" });
+          const textContent = await page.evaluate(() => {
+            const bodyElement = document.querySelector("body");
+            return bodyElement ? bodyElement.textContent : "";
+          });
+          return textContent || "";
+        } catch (error) {
+          console.error("Ocurrió un error al cargar la página", error);
+          return "";
+        }
+      },
+      browser, // Pasar el navegador al loader
+    });
+
+    // Cargar datos
+    return await loader.load();
+  } catch (error) {
+    console.error("Ocurrió un error al iniciar el navegador", error);
+    return "";
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
 };
