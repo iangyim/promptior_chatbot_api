ad0dae6a1ebdc77d721a589bf35fa54ac3a46713
Iang Yim <iang.yim@uruit.com>
2024-03-22 23:56:42 -0300

    testing chrome-aws

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,9 +1,20 @@
-const { join } = require("path");
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+exports.handler = async (event) => {
+    let browser = null;
+    try {
+        browser = await puppeteer.launch({
+            executablePath: await chromium.executablePath,
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            headless: chromium.headless,
+        });
+
+        // Your Puppeteer code here
+    } finally {
+        if (browser !== null) {
+            await browser.close();
+        }
+    }
 };
